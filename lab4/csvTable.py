337293cd026b22c1226572991f197020914be9c1
Tolstykh Maksym <mrymnik3d@gmail.com>
2024-08-24 23:47:15 +0300

    refactor csvTable

--- lab4/csvTable.py
+++ lab4/csvTable.py
@@ -3,33 +3,40 @@ from faker import Faker
 
 fake = Faker('uk-UA')
 
-headers = ["Прізвище","Ім’я","По батькові","Стать","Дата народження","Посада","Місто проживання","Адреса проживання","Телефон","Email"]
+headers = ["Прізвище", "Ім’я", "По батькові", "Стать", "Дата народження", "Посада", "Місто проживання", "Адреса проживання", "Телефон", "Email"]
 
 # Генеруємо дані
 def generate_fake_data(num_rows):
     data = []
-    for _ in range(num_rows):
-        gender = fake.random_element(elements=('Чоловік', 'Жінка'))
-        # Генерація імені відповідно до статі
+    num_male = int(num_rows * 0.6)  # 60% чоловіків
+    num_female = num_rows - num_male  # 40% жінок
+
+    def generate_person(gender):
         if gender == 'Чоловік':
             first_name = fake.first_name_male()
             middle_name = fake.middle_name_male()
         else:
             first_name = fake.first_name_female()
             middle_name = fake.middle_name_female()
-        row = [
-            fake.last_name(),            # Прізвище
-            first_name ,           # Ім’я
-            middle_name,          # По батькові
-            gender,  # Стать
+        
+        return [
+            fake.last_name(),  # Прізвище
+            first_name,        # Ім’я
+            middle_name,       # По батькові
+            gender,            # Стать
             fake.date_of_birth(minimum_age=16, maximum_age=86).strftime('%d-%m-%Y'),  # Дата народження
-            fake.job(),                  # Посада
-            fake.city(),                 # Місто проживання
+            fake.job(),        # Посада
+            fake.city(),       # Місто проживання
             fake.address().replace('\n', ', '),  # Адреса проживання
-            fake.phone_number(),         # Телефон
-            fake.email()                 # Email
+            fake.phone_number(),  # Телефон
+            fake.email()       # Email
         ]
-        data.append(row)
+
+    # Додаємо записи для чоловіків
+    data.extend(generate_person('Чоловік') for _ in range(num_male))
+    # Додаємо записи для жінок
+    data.extend(generate_person('Жінка') for _ in range(num_female))
+    
     return data
 
 # Генеруємо фейкові дані
